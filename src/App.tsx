38ccdf1b68c31005beeef6daedf7c45553d719e1
Marcus A. Schnake <marcus.schnake@gmail.com>
2022-02-14 23:18:32 -0500

    still fixing Heroku

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-require('dotenv').config();
+// require('dotenv').config();
 import { useState } from 'react';
 import Navbar from './components/NavbarForm';
 import LandingPage from './pages/LandingPage';
